72128e3249ea5e0e1f495fa8d01e63e74387ac37
inesdrb <derbali.ines1@gmail.com>
2025-06-03 12:48:57 +0200

    third commit

--- app/components/details.js
+++ app/components/details.js
@@ -5,30 +5,24 @@ import Image from "next/image";
 
 export default function DetailsLogementCard({ logement }) {
   return (
-    <div className="bg-color-text rounded-lg shadow-md overflow-hidden">
-        
-    <div className="p-4">
-        <h2 className="text-xl font-semibold font-title">{logement.nom}</h2>
-
-        </div>
-
+ <div className="bg-white rounded-lg shadow-md ">
       <Image
         src={logement.image}
         alt={logement.nom}
-        width={400}
-        height={300}
+        width={300}
+        height={200}
         className="w-full h-48 object-cover"
-      />
-      <div className="p-4">
-        <p className="text-lg font-bold text-button mt-2">{logement.prix}</p>
-   <p className="text-sm text-gray-600 font-body">Description : {logement.description}</p>
-        <p className="text-sm text-gray-600 font-body">Catégorie : {logement.categorie}</p>
-        
+      />   
+       <div className="p-4">
+        <h2 className="text-xl font-semibold font-opensans text-primary">{logement.nom}</h2>
+        <p className="text-sm text-blue-900 font-body">Catégorie : {logement.categorie}</p>
+        <p className="text-lg font-bold text-secondary mt-2">{logement.prix}</p>
       </div>
+    
 <button
         onClick={() => {
     alert('Reservation confirmée');} }
-        className="bg-primary text-color-text px-4 py-2 rounded hover:bg-secondary transition ml-16 mr-16 mb-16 "
+        className="bg-primary text-secondary px-4 py-2 rounded hover:bg-secondary transition ml-16 mr-16 mb-16 font-bold font-opensans "
       >
         Réserver
       </button>
--- app/components/header.js
+++ app/components/header.js
@@ -10,5 +10,5 @@ export default function Header() {
       </div>
    </div>
   );
-}
+} 
 
